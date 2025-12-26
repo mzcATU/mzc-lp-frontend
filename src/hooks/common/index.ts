e9f8abc51cb20846438ca02c006dd27e26c26399
shin-mz <ggg9905@mz.co.kr>
2025-12-26 14:31:46 +0900

    feat: useAuthQueries 훅 및 401 자동 로그아웃 처리 추가 (#39)
    - useAuthQueries 훅 생성 (useLogin, useRegister, useLogout, useRefreshToken, useMe)
    - axiosInstance에 401 에러 시 토큰 갱신 및 자동 로그아웃 로직 추가

--- src/hooks/common/index.ts
+++ src/hooks/common/index.ts
@@ -1,3 +1,23 @@
 export { useDebounce } from './useDebounce';
 export { useLocalStorage } from './useLocalStorage';
 export { useIsMobile } from './useIsMobile';
+
+// Auth Hooks
+export {
+  authKeys,
+  useMe,
+  useLogin,
+  useRegister,
+  useLogout,
+  useRefreshToken,
+} from './useAuthQueries';
+
+// User Hooks
+export {
+  userKeys,
+  useMyProfile,
+  useUpdateProfile,
+  useChangePassword,
+  useWithdraw,
+  useUploadProfileImage,
+} from './useUserQueries';
