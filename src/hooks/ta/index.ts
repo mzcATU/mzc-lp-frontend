c19406d164c7e6c7fb899809a30d178084272e1c
shin-mz <ggg9905@mz.co.kr>
2026-01-04 01:29:08 +0900

    대시보드 모듈 export 추가로 TypeScript 컴파일 에러 수정
    - hooks/ta/index.ts에 dashboardKeys, useTaDashboardKpi export 추가
    - services/ta/index.ts에 dashboardService export 추가
    - CI 테스트 실패 해결

--- src/hooks/ta/index.ts
+++ src/hooks/ta/index.ts
@@ -28,3 +28,8 @@ export {
   useUpdateBranding,
   useUpdateUserManagement,
 } from './useTenantSettingsQueries';
+
+export {
+  dashboardKeys,
+  useTaDashboardKpi,
+} from './useDashboardQueries';
--- src/services/ta/index.ts
+++ src/services/ta/index.ts
@@ -1,3 +1,4 @@
 export { userService } from './userService';
 export { groupService } from './groupService';
 export { tenantSettingsService } from './tenantSettingsService';
+export { dashboardService } from './dashboardService';
